e1b6551a4a52065a8dfd899bd94a5955f3fe40ba
nikolyaFilippov <nikolya_filippov_88@mail.ru>
2023-08-25 14:18:14 +0300

    blank

---
+++ README.md
@@ -0,0 +1,9 @@
+# HOW TO START
+In Idea 
+mvn clean compile
+start as app in Idea
+
+As a jar
+mvn clean package
+
+start as a jar
---
+++ src/main/java/com/training/documentprocess/DocumentprocessApplication.java
@@ -0,0 +1,13 @@
+package com.training.documentprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentprocessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentprocessApplication.class, args);
+	}
+
+}
